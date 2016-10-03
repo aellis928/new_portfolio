e7b439a898b236536864dbb7b547812288ad9151
aellis928 <mrskawasaki.90@gmail.com>
2016-10-02 21:12:42 -0700

    finished home page

--- new.js
+++ new.js
@@ -26,7 +26,7 @@ function change_tab(event, tab_name){
 
      tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace("active", "");
     }
 
     document.getElementById(tab_name).style.display = "block";
